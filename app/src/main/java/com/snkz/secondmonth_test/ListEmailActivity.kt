13f771a58053d6877a1a66f282e63d4e0ad994ea
SNKz0610 <blackaxe2202@gmail.com>
2021-06-23 22:54:02 +0700

    Adding all the files for this excercise. Because so many trash file, I just push neccesary files.

--- app/src/main/java/com/snkz/secondmonth_test/ListEmailActivity.kt
+++ app/src/main/java/com/snkz/secondmonth_test/ListEmailActivity.kt
@@ -1,4 +1,34 @@
 package com.snkz.secondmonth_test
 
-class ListEmailActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.snkz.secondmonth_test.databinding.ActivityListEmailBinding
+import com.snkz.secondmonth_test.databinding.ActivityMainBinding
+
+class ListEmailActivity : AppCompatActivity() {
+    private lateinit var listEmailBinding: ActivityListEmailBinding
+    private var arrMail : ArrayList<Mail> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        listEmailBinding = DataBindingUtil.setContentView(this, R.layout.activity_list_email)
+        createListMails()
+
+        listEmailBinding.revListemailMail.adapter = MailAdapter(arrMail)
+        listEmailBinding.revListemailMail.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createListMails() : ArrayList<Mail>{
+        arrMail.add(Mail(R.drawable.avatar1, "Nguyen Manh Duy", "Deadline ngay mai", "Lmao lam bai tap chua", "June 23"))
+        arrMail.add(Mail(R.drawable.avatar2, "Nguyen Hong Son", "Deadline PTTK", "Game la de~", "June 22"))
+        arrMail.add(Mail(R.drawable.avatar3, "Pham Thanh Binh", "Go pro ko?", "Chieu mai ban onlan k?", "June 18"))
+        arrMail.add(Mail(R.drawable.avatar4, "Le Viet Linh", "Tuyen pro go major", "Thieu 1 slot stack 5 kia`", "June 5"))
+        arrMail.add(Mail(R.drawable.avatar5, "Thay Que", "Het han deadline", "O. Ka", "June 2"))
+        return arrMail
+    }
 }
--- app/src/main/java/com/snkz/secondmonth_test/MailAdapter.kt
+++ app/src/main/java/com/snkz/secondmonth_test/MailAdapter.kt
@@ -1,4 +1,44 @@
 package com.snkz.secondmonth_test
 
-class MailAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.snkz.secondmonth_test.databinding.ItemListemailMailBinding
+
+class MailAdapter(val arrMails: ArrayList<Mail>) : RecyclerView.Adapter<MailAdapter.Viewholder>() {
+    private var statusFarvorite = 1;
+
+    class Viewholder(val binding: ItemListemailMailBinding) : RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MailAdapter.Viewholder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemListemailMailBinding.inflate(layoutInflater, parent, false)
+        return Viewholder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MailAdapter.Viewholder, position: Int) {
+        val mail: Mail = arrMails.get(position)
+        if (mail != null) {
+            holder.binding.imageItemListemailAvatar.setImageResource(mail.avatar)
+            holder.binding.textItemListemailName.text = mail.name
+            holder.binding.textItemListemailDes.text = mail.des
+            holder.binding.textItemListemailContent.text = mail.content
+            holder.binding.textItemListemailDate.text = mail.time
+            holder.binding.imagaeListemailFarvorite.setOnClickListener {
+                statusFarvorite += 1
+                if (statusFarvorite % 2 == 0) {
+                    holder.binding.imagaeListemailFarvorite.setImageResource(R.drawable.ic_baseline_star_ticked_24)
+                } else {
+                    holder.binding.imagaeListemailFarvorite.setImageResource(R.drawable.ic_baseline_star_border_24)
+                }
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        if (arrMails != null) {
+            return arrMails.size
+        }
+        return 0
+    }
 }
